988a11c1704df3b8b6d6eab22a32ee590e4bb5d7
Advyta <advytajs@gmail.com>
2024-03-10 14:53:47 +0100

    modifications

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+// index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, createHashRouter, createRoutesFromElements } from 'react-router-dom'; // Import HashRouter
 import Layout from './Pages/Layout/Layout';
 import Home from './Pages/Home/Home';
 import Crew from './Pages/Crew/Crew';
@@ -12,22 +13,22 @@ import Destination from './Pages/Destination/Destination';
 import NotFoundPage from './Pages/NotFoundPage';
 import data from './assets/shared/data.json';
 
-const router = createBrowserRouter(
+const router = createHashRouter( // Use HashRouter
   createRoutesFromElements(
-    <Route path='/' element = {<Layout/>}>
+    <Route path='/' element={<Layout />}>
       <Route path='' element={<Home />} />
-      <Route path='Destination' element={<Destination data = {data.destinations}/>} />
-      <Route path='Crew' element={<Crew data = {data.crew}/>} />
-      <Route path='Technology' element={<Technology data = {data.technology}/>} />
+      <Route path='Destination' element={<Destination data={data.destinations} />} />
+      <Route path='Crew' element={<Crew data={data.crew} />} />
+      <Route path='Technology' element={<Technology data={data.technology} />} />
       <Route path='*' element={<NotFoundPage />} /> {/* Handle not-found scenario */}
     </Route>
   )
-)
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 reportWebVitals();
